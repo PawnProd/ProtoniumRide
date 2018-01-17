a30b808927ab567cf2d30d04b1ef5f38d0f5550c
Pawn Production <nathan.prospert@pawnproduction.fr>
2018-01-17 17:43:57 +0100

    Début shop module

---
+++ Assets/Scripts/AchatPanel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchatPanel : MonoBehaviour {
+
+    /// <summary>
+    /// L'image représentant l'objet
+    /// </summary>
+    public Image objImg;
+
+    /// <summary>
+    /// L'image de la catégorie de l'objet
+    /// </summary>
+    public Image categorieImg;
+
+    /// <summary>
+    /// Le nom de l'objet
+    /// </summary>
+    public Text objName;
+
+    /// <summary>
+    /// Le prix de l'objet
+    /// </summary>
+    public Text objPrice;
+
+    /// <summary>
+    /// Change l'image de l'objet dans le shop
+    /// </summary>
+    /// <param name="objSprite"> La nouvelle image de l'objet</param>
+	public void SetObjImg(Sprite objSprite)
+    {
+        objImg.sprite = objSprite;
+    }
+
+    /// <summary>
+    /// Change l'image de la catégorie de l'objet dans le shop
+    /// </summary>
+    /// <param name="categorieSprite"> La nouvelle image de la catégorie</param>
+    public void SetCategorieImg(Sprite categorieSprite)
+    {
+        categorieImg.sprite = categorieSprite;
+    }
+
+    /// <summary>
+    /// Change le nom de l'objet dans le shop
+    /// </summary>
+    /// <param name="name"> Le nouveau nom de l'objet</param>
+    public void SetObjName(string name)
+    {
+        objName.text = name;
+    }
+
+    /// <summary>
+    /// Change le prix de l'objet dans le shop
+    /// </summary>
+    /// <param name="price"> Le nouveau prix de l'objet</param>
+    public void SetObjPrice(string price)
+    {
+        objPrice.text = price;
+    }
+}
